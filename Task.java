5f53ce8493752ca2e4bdbd7f11f35a6ebed6bf1a
Pandore10 <lipefmenezes@gmail.com>
2026-09-12 20:18:52 -0300

    feat: adicionando a classe Task

---
+++ Task.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class Task {
+        private final int lastId = 1;
+
+        private int id;
+        private String description;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+}
