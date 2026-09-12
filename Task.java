60c8e98f66523bdeb097d170f494e202dc150641
Pandore10 <lipefmenezes@gmail.com>
2026-09-12 20:55:11 -0300

    feat: adicionar toString

--- Task.java
+++ Task.java
@@ -1,10 +1,28 @@
 import java.time.LocalDateTime;
 
 public class Task {
-        private final int lastId = 1;
+        private static int lastId = 0;
 
         private int id;
         private String description;
+        private String status;
         private LocalDateTime createdAt;
         private LocalDateTime updatedAt;
+        
+        public Task(String description) {
+            this.id = ++lastId;
+            this.description = description;
+            this.status = "todo";
+            this.createdAt = LocalDateTime.now();
+            this.updatedAt = LocalDateTime.now();
+        }
+
+        @Override 
+        public String toString() {
+            return String.format("Task ID: %d\n" + 
+                                 "Task Description: %s\n" +
+                                 "Task Status: %s\n" +
+                                 "Last updated: %s\n" +
+                                 "Created: %s", this.id, this.description, this.status, this.updatedAt, this.createdAt);
+        }
 }
