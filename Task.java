50b62295873cd797fb3865a355f041b059e7b182
Pandore10 <lipefmenezes@gmail.com>
2026-09-21 21:40:28 -0300

    feat: Task class

---
+++ Task.java
@@ -0,0 +1,19 @@
+import java.time.LocalDateTime;
+
+public class Task {
+    public static int lastId = 0;
+
+    private int id;
+    private String description;
+    private Status status;
+    private LocalDateTime updatedAt;
+    private LocalDateTime createdAt;
+
+    public Task(String description) {
+        this.id = ++lastId;
+        this.description = description;
+        this.status = Status.TODO;
+        this.updatedAt = LocalDateTime.now();
+        this.createdAt = LocalDateTime.now();
+    }
+}
