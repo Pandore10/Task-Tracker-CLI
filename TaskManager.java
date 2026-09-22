62e71da717b866ec9948a1cb54ec3c9fe6a5e751
Pandore10 <lipefmenezes@gmail.com>
2026-09-21 22:43:47 -0300

    feat: create and delete methods in task handler class

--- TaskManager.java
+++ TaskManager.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskManager {
-    private List<Task> tasks;
+    private List<Task> tasks = new ArrayList<>();
+
+    public void createTask(String description) {
+        Task task = new Task(description);
+        tasks.add(task);
+    }
+
+    public void deleteTask(int id) {
+        for (Task task : tasks) {
+            if (task.getId() == id) tasks.remove(task);
+        }
+    }
 }
